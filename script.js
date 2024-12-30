eace0c5b76ac7942f87ffb9591d82b42bfaa8352
Ramesh <rameshpotaliya@gmail.com>
2024-12-30 22:50:01 +0530

    chatbot added

--- script.js
+++ script.js
@@ -57,10 +57,10 @@ $(document).ready(function() {
     ScrollReveal().reveal(".header ul, .profile-text, .about-skills, .internship", {
       origin: "right"
     });
-    ScrollReveal().reveal(".project-title, .contact-title", {
+    ScrollReveal().reveal(" .skill-title, .project-title, .contact-title", {
       origin: "top"
     });
-    ScrollReveal().reveal(".projects, .contact", {
+    ScrollReveal().reveal("#skills, .projects, .contact", {
       origin: "bottom"
     });
 
